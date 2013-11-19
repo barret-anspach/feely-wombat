20aecefb1f1344bcd4921bc29510cd459ec4ca6f
barret-anspach <wba@barretanspach.com>
2013-11-19 11:47:42 -0800

    Updated some things...!??

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,43 +1,45 @@
 'use strict';
 
 angular.module('schyllingApp')
-    .factory('Catalog', function() {
-
-        var Catalog = Parse.Object.extend("Items", {
-            // Instance methods
-        }, {
-            // Class methods
-        }
-        );
-
-        // Title property
-        Presentation.prototype.__defineGetter__("title", function() {
-            return this.get("title");
-        });
-        Presentation.prototype.__defineSetter__("title", function(aValue) {
-            return this.set("title", aValue);
-        });
-
-        return Presentation;
-    })
+//    .factory('Catalog', function() {
+//
+//        var Catalog = Parse.Object.extend("Items", {
+//            // Instance methods
+//        }, {
+//            // Class methods
+//        }
+//        );
+//
+//        // Title property
+//        Catalog.prototype.__defineGetter__("productTitle", function() {
+//            return this.get("title");
+//        });
+//        Catalog.prototype.__defineSetter__("productTitle", function(aValue) {
+//            return this.set("title", aValue);
+//        });
+//
+//        return Catalog;
+//    })
 
     .controller('MainCtrl', ['$scope', 'ParseService', function($scope, ParseService) {
-        $scope.headline = "Artisan of Italy";
+        $scope.headline = {
+            title: "Artisan of Italy", subtitle: "Handmade Italian watch straps."
+        };
 
         $scope.strap = Parse.Object("Strap");
         $scope.strapCollection = Parse.Collection({model:$scope.strap});
 
-        $scope.query = new Parse.Query()
+        $scope.itemQuery = new Parse.Query();
 
 //        $scope.collectionQuery = new Parse.Query("Strap");
 
         $scope.itemQuery.equalTo("ASIN", "B009PP1IYI");
         $scope.itemQuery.find({
             success: function(items) {
-                console.log(items[0].attributes);
-              angular.forEach(items, function(v, i){
-                  $scope.items.push(v.id);
-              })
+                angular.forEach(items, function(v, i){
+                    console.log(items[0].attributes);
+                    $scope.items.push(v.id);
+                })
 
             },
             error: function(error) {
--- app/scripts/services/monocle.coffee
+++ app/scripts/services/monocle.coffee
@@ -21,9 +21,7 @@ strapApp.factory('monocle', ['$http', 'parseWrapper', ($http, parseWrapper) ->
 			$http.defaults.headers.common['X-Parse-Application-Id'] = 'vsph4BARvMHNuQJoRU1lDxhxg1otvLCa4ArSrERA'    # beat-1
 			$http.defaults.headers.common['X-Parse-REST-API-Key'] = '9scmvh4r75QgFKiWau9nwyhXzwt2z18AQu1D3pKA'      # beat-1
 
-
 	monocle.Network = parseWrapper.Resource.registerResource(Strap)
-
 
 	monocle.initialize()
 
