08236acdf7f834b1517fef0ecfe7e1571b6d497b
sethgarrison <seth.garrison@rainsparklabs.com>
2013-11-19 12:54:43 -0800

    added a new parse wrapper for you to play with -- completely restful -- added some examples

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,28 +21,79 @@ angular.module('schyllingApp')
         return Presentation;
     })
 
-    .controller('MainCtrl', ['$scope', 'ParseService', function($scope, ParseService) {
+    .controller('MainCtrl', ['$scope', 'ParseService', 'monocle', function($scope, ParseService, monocle) {
         $scope.headline = "Artisan of Italy";
 
-        $scope.strap = Parse.Object("Strap");
-        $scope.strapCollection = Parse.Collection({model:$scope.strap});
+//			You can do queries on any class you have set up on parse and in your monocle wrapper like this
+			$scope.strapQuery = monocle.Strap.query();
 
-        $scope.query = new Parse.Query()
 
-//        $scope.collectionQuery = new Parse.Query("Strap");
+//			These queries have to be .fetch() ed to get data from them  -- this fetch method returns a success and error
+//			call back like so == also, the response to the query is always added to the query object as $scope.queryObject.items
+//			So you can reference that in an ng repeat
+			$scope.strapQuery.fetch().success(function(data){
+//				process data with and angular.forEach --- or just leave it alone -- just remember that data.items is the list
+//				of results from Parse
+				console.log(data);
+			}).error(function(err){
+//						hadle error
+						console.log(err)
+					});
 
-        $scope.itemQuery.equalTo("ASIN", "B009PP1IYI");
-        $scope.itemQuery.find({
-            success: function(items) {
-                console.log(items[0].attributes);
-              angular.forEach(items, function(v, i){
-                  $scope.items.push(v.id);
-              })
+//			To load a single document/object (ie strap) from your collection (ie Strap) initialize it by calling a
+//      new monocle.ClassName(id):
 
-            },
-            error: function(error) {
-                alert("Error: " + error.code + " " + error.message);
-            }
-        });
+			$scope.strap = new monocle.Strap("pZYTheRo8t");
+
+//			Then load that object like so -- very similarly to the way you .fetch() the query
+			$scope.strap.load().success(function(data){
+				console.log(data);
+			}).error(function(err){
+						console.log(err);
+					});
+
+//			Check out the ParseWrapper.coffee to see other available object or query methods...
+//			For instance, you can save a strap by:
+
+			$scope.strapNew = new monocle.Strap();
+
+			$scope.strap.type = 'Plastic';
+			$scope.strap.name = 'legos';
+
+
+			$scope.saveStrap = function(strap){
+				strap.specificsChanged = false;
+				strap.save().success(function(data){
+					console.log('saved strap successfully');
+					$scope.message = "Saved Strap Successfully"
+				})
+			};
+
+
+
+
+
+//
+
+//        $scope.strap = Parse.Object("Strap");
+//        $scope.strapCollection = Parse.Collection({model:$scope.strap});
+//
+//        $scope.query = new Parse.Query()
+//
+////        $scope.collectionQuery = new Parse.Query("Strap");
+//
+//        $scope.itemQuery.equalTo("ASIN", "B009PP1IYI");
+//        $scope.itemQuery.find({
+//            success: function(items) {
+//                console.log(items[0].attributes);
+//              angular.forEach(items, function(v, i){
+//                  $scope.items.push(v.id);
+//              })
+//
+//            },
+//            error: function(error) {
+//                alert("Error: " + error.code + " " + error.message);
+//            }
+//        });
 
     }]);
--- app/scripts/services/monocle.coffee
+++ app/scripts/services/monocle.coffee
@@ -3,12 +3,17 @@ strapApp = angular.module('schyllingApp');
 strapApp.factory('monocle', ['$http', 'parseWrapper', ($http, parseWrapper) ->
 
 
+
+#	This defines your CLASSES -- which is a type of object in your Parse Data Browser
+#	Just copy this code exactly when you want to keep track of a new class, just change the name of it and the properties
+#	Then add it to the monocle object below by typing monocle.ClassName = parseWrapper.Resource.registerResource(ClassName)
 	class Strap extends parseWrapper.Resource
 		constructor: (objectId) ->
 			super(objectId)
 
 		@parseClassName: "Strap"
 		@register: () ->
+			#this is where you define your classes properities -- or column names
 			name: undefined,
 			type: undefined,
 			quantity: undefined,
@@ -22,7 +27,10 @@ strapApp.factory('monocle', ['$http', 'parseWrapper', ($http, parseWrapper) ->
 			$http.defaults.headers.common['X-Parse-REST-API-Key'] = '9scmvh4r75QgFKiWau9nwyhXzwt2z18AQu1D3pKA'      # beat-1
 
 
-	monocle.Network = parseWrapper.Resource.registerResource(Strap)
+#	This is where you define your service === it's defining monocle.Strap so that you can access from any controller
+#	That you inject monocle in -- so if you add a new class above, make sure to copy the following line and substituting
+#	Whatever ClassName you used
+	monocle.Strap = parseWrapper.Resource.registerResource(Strap)
 
 
 	monocle.initialize()
